b8522ca8f16fb92259dac4b8a27485dcd14d457d
blsoko <exanymejose1@hotmail.com>
2020-12-07 16:17:19 -0500

    correct lowercase

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Create a file
 file { '/tmp/holberton':
   ensure  => 'file',
-  content => 'I Love Puppet',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
