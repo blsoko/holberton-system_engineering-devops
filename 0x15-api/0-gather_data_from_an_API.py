20368e1e218b55337099ad6b8b72705fd2cba500
blsoko <exanymejose1@hotmail.com>
2021-02-01 16:05:12 -0500

    json data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Gather data from HTTP Response """
-from sys import argv
 import json
 import requests
+from sys import argv
+
 
 if __name__ == "__main__":
     empName = ""
